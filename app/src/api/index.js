8e9afd8e80f782ee6f64f5fb98e1f59a4532b93a
“Sourav <elsesourav@gmail.com>
2025-08-18 00:25:30 +0530

    text to run new way for voice action

--- app/src/api/index.js
+++
@@ -1,6 +0,0 @@
-// Export all API functions
-export * from "./dataStoreApi";
-export * from "./ordersApi";
-export * from "./productGroupsApi";
-export * from "./productsApi";
-export * from "./usersApi";
--- app/src/components/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-import { HomeScreen } from "../screens/HomeScreen";
-
-export const HomePage = ({ navigation }) => {
-   return <HomeScreen navigation={navigation} />;
-};
